cc536e3dc77a54d70cce3bc5006e44ebdebce157
Liz <51245710+eahowell@users.noreply.github.com>
2024-05-27 16:33:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com", "http://localhost:1234"];
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com", "http://localhost:1234", "https://wtp8hh.csb.app"];
 
 app.use(
   cors({
