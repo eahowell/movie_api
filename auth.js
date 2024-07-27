0427ce3d3a1436b803221e166365f92940628fac
Liz <51245710+eahowell@users.noreply.github.com>
2024-07-26 20:32:41 -0500

    Split Auth and Validate into 2 JS Files

--- auth.js
+++ auth.js
@@ -33,28 +33,3 @@ module.exports = (router) => {
     })(req, res);
   });
 };
-
-// READ - GET - Validate provided password is correct;  (using username and password)
-module.exports = (router) => {
-  router.get("/validation", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
-      if (error) {
-        return res.status(400).json({
-          message: "Something is not right",
-          user: false,
-        });
-      } else if (!user) {
-        return res.status(400).json({
-          message: info.message,
-          user: false,
-        })
-      } else {
-        return res.status(200).json({
-          message: "Valid password",
-          user: user,
-        });
-      }
-    })(req, res);
-  });
-};
-
---
+++ validation.js
@@ -0,0 +1,29 @@
+// Description: Validate provided password is correct;  (using username and password)
+  const passport = require("passport"); // Local passport file
+
+
+
+// READ - GET - Validate provided password is correct;  (using username and password)
+module.exports = (router) => {
+  router.get("/validation", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
+      if (error) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: false,
+        });
+      } else if (!user) {
+        return res.status(400).json({
+          message: info.message,
+          user: false,
+        })
+      } else {
+        return res.status(200).json({
+          message: "Valid password",
+          user: user,
+        });
+      }
+    })(req, res);
+  });
+};
+
