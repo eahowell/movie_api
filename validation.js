89f21cc5e15ad12410fc0c1546eb151da6d21445
Liz <51245710+eahowell@users.noreply.github.com>
2024-07-26 21:35:43 -0500

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,6 +1,7 @@
 // Description: Validate provided password is correct;  (using username and password)
   const Models = require("./models.js");  
   const Users = Models.User;
+  const passport = require("passport");
 
 // CREATE - POST - Validate provided password is correct;  (using username and password)
 // READ - GET - Validate provided password is correct;  (using username and password)
@@ -10,10 +11,10 @@ module.exports = (router) => {
     .then((user) => {
       if (user) {
         let hashPassword = Users.hashPassword(req.body.Password);
-        if (hashPassword === user.Password) {
+        if (user.validatePassword(req.body.Password)) {
           res.status(200).send(true);
         } else {
-          res.status(401).send("Password was not correct." + user.Password + " Hashed:" + hashPassword);
+          res.status(401).send("Password was not correct." + user.Password + " Hashed:" + hashPassword + " fggg"+ user.validatePassword(req.body.Password));
         }        
       } else {
         res
