5ea719181cf6e81667ce1c361281466d9f7644a1
Liz <51245710+eahowell@users.noreply.github.com>
2024-07-26 21:38:12 -0500

    Cleaned up error message

--- validation.js
+++ validation.js
@@ -1,7 +1,6 @@
 // Description: Validate provided password is correct;  (using username and password)
   const Models = require("./models.js");  
   const Users = Models.User;
-  const passport = require("passport");
 
 // CREATE - POST - Validate provided password is correct;  (using username and password)
 // READ - GET - Validate provided password is correct;  (using username and password)
@@ -10,11 +9,10 @@ module.exports = (router) => {
     Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
-        let hashPassword = Users.hashPassword(req.body.Password);
         if (user.validatePassword(req.body.Password)) {
           res.status(200).send(true);
         } else {
-          res.status(401).send("Password was not correct." + user.Password + " Hashed:" + hashPassword + " fggg"+ user.validatePassword(req.body.Password));
+          res.status(401).send("Password was not correct.");
         }        
       } else {
         res
